d75932a5f48b3143b81b8b14d1e612a6de549258
Alexej Holad <alexej.holad@gmail.com>
2021-12-29 18:15:17 +0100

    Plus One solved with Pallavi

---
+++ 100InterviewQuestions/Plus-One.js
@@ -0,0 +1,17 @@
+const plusOne = (digits) => {
+    for (let i = 0; i < digits.length; i++) {
+      let ifNine = digits[digits.length - i - 1];
+      if (ifNine === 9) {
+        digits[digits.length - i - 1] = 0;
+      } else {
+        digits[digits.length - i - 1] += 1;
+        return digits;
+      }
+    }
+    if (digits[0] === 0) {
+      digits.unshift(1);
+    }
+    return digits;
+  };
+  
+  console.log(plusOne([9, 9]));

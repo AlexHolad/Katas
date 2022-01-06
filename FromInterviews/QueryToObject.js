86ee82999f73a7fcbd9cff43b60468e0b870ad2f
Alexej Holad <alexej.holad@gmail.com>
2022-01-06 20:58:40 +0100

    Query Objectify

---
+++ FromInterviews/QueryToObject.js
@@ -0,0 +1,42 @@
+const inData = "user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue&experiments.theme=dark"
+
+const queryObjectify = (str) => {
+  let res = {};
+  str = str.split("&");
+  str = str.map((i) => i.split("."));
+  for (let i = 0; i < str.length; i++) {
+    let cur = res;
+    for (let key = 0; key < str[i].length; key++) {
+      let name = str[i][key];
+      if (key == str[i].length - 1) {
+        name = name.split("=");
+        cur[name[0]] = decodeURIComponent(name[1]);
+        break;
+      }
+      if (cur[name]) {
+        cur = cur[name];
+      } else {
+        cur[name] = {};
+        cur = cur[name];
+      }
+    }
+  }
+
+  return res;
+};
+
+console.log(queryObjectify(
+  inData
+));
+// {
+//   'user': {
+//     'name': {
+//       'firstname': 'Bob',
+//       'lastname': 'Smith'
+//     },
+//     'favoritecolor': 'Light Blue'
+//   },
+//   experiments: {
+//     theme: 'dark'
+//   }
+// }
